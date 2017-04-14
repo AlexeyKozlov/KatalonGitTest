5ae0b886d8d022193086385f4c04252940d167fa
Alexey Kozlov <alexeymspm@gmail.com>
2017-04-14 08:29:12 -0400

    test message

---
+++ Object Repository/Page_Russian Easter - Google Search/div_April 16.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_April 16</name>
+   <tag></tag>
+   <elementGuidId>a78d85e9-d672-4e36-9d72-04df3bbfd611</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;_NId&quot;]/div[@class=&quot;g mnr-c g-blk&quot;]/div[@class=&quot;kp-blk _Rqb _RJe&quot;]/div[@class=&quot;xpdopen _vcj&quot;]/div[@class=&quot;_OKe&quot;]/div[2]/div[@class=&quot;_Wtj mod&quot;]/div[1]/div[@class=&quot;vk_gy vk_sh whenis&quot;]/div[@class=&quot;vk_bk vk_ans&quot;]/div[2]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>April 16</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_Russian Easter - Google Search/div_russian easter.rs
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_russian easter</name>
+   <tag></tag>
+   <elementGuidId>a657cb19-b08c-4111-98c2-35177d769498</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;sbse0&quot;)/div[@class=&quot;sbqs_c&quot;]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>russian easter</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sbqs_c</value>
+   </webElementProperties>
+</WebElementEntity>
